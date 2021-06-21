ee6e0bdb5b0266c9c77aba712af2062d1eef8686
Xin Xue <xin.xue@happyelements.com>
2021-06-21 00:02:58 -0400

    kmeans update

---
+++ ML_algos_from_scratch/kmeans.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class kmeans:
+    def __init__(self, K):
+        self.K = K
+
+    def kmeans(self, X):
+        return None
+
+    def kmedian(self, X):
+        return None
+        
+    def convergence(self, tol):
+        return None
+
