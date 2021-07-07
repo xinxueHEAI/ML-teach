d9ae0d9d36f809eaacb9edaaf70806540e25328e
Xin Xue <xin.xue@happyelements.com>
2021-07-06 23:11:46 -0400

    code update

--- ML_algos_from_scratch/ols.py
+++ ML_algos_from_scratch/ols.py
@@ -29,10 +29,26 @@ class OLS:
             X_sd = np.std(X, axis = 0)
             X = (X - X_mean)/X_sd
         return X
+    
+    def G_l1(self, X, beta, y,  lambdaa):
+        return 2 * X.T * X * beta - 2 * X.T * y + lambdaa * np.where(beta>0,1,-1)
+    
+    def G_l2(self, X, beta, lambdaa):
+        return 2 * X.T * X * beta - 2 * X.T * y + 2 * lambdaa * beta
+    
+    def loss(self, X, w, y, lambdaa):
+        if self.regularization == 'l1':
+            return (y - X * w).T * (y - X * w) + lambdaa * np.sum(np.abs(w))
+        elif self.regularization == 'l2':
+            return (y - X * w).T * (y - X * w) + lambdaa * np.sum(w.T * w)
+        else:
+            return (y - X * w).T * (y - X * w)
+
+    def GD(self, X, w, y, lambdaa, lr = 0.001, tol = 1e-5):
+        init_loss = self.loss(X, w, y, lambdaa)
+        new_w = 
+        while
         
-    def _gd(gradient):
-        def wrapped(self, *args, **kwargs):
-            gradients
             
         
         
